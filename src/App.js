271ce2053730dee706dcf645f96a953a106a73a3
py <pyd3.14@gmail.com>
2022-07-23 21:39:25 +0200

    Add CV

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FaceIcon from '@mui/icons-material/Face';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import Presentation from './components/Presentation';
 import Skills from './components/Skills';
+import Cv from './components/CV';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -53,10 +54,11 @@ export default function BasicTabs() {
       <Box sx={{ width: '100%' }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label={<FaceIcon />} {...a11yProps(0)} className="tab-label" />
-            <Tab label={<ListAltIcon />} {...a11yProps(1)} className="tab-label" />
-            <Tab label="X" {...a11yProps(2)} className="tab-label" />
-            <Tab label="D" {...a11yProps(3)} className="tab-label" />
+            <Tab label={<FaceIcon />} {...a11yProps(0)} className="tab-label" style={{ minWidth: "10%" }} />
+            <Tab label={<ListAltIcon />} {...a11yProps(1)} className="tab-label" style={{ minWidth: "10%" }} />
+            <Tab label="CV" {...a11yProps(2)} className="tab-label" style={{ minWidth: "10%" }} />
+            <Tab label="D" {...a11yProps(3)} className="tab-label" style={{ minWidth: "10%" }} />
+            <Tab label="E" {...a11yProps(4)} className="tab-label" style={{ minWidth: "10%" }} />
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
@@ -66,7 +68,7 @@ export default function BasicTabs() {
           <Skills />
         </TabPanel>
         <TabPanel value={value} index={2}>
-          Item Three
+          <Cv />
         </TabPanel>
       </Box>
     </div>
